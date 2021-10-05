fdf469d0b7e44ce4f7446196455d42c72a728a70
EmersonPaduan <emerson.paduan@gmail.com>
2021-10-05 09:40:49 -0300

    aula 06 - exercicio relogio

--- aula06/exercicios/exercicio04/AppRelogio.java
+++ aula06/exercicios/exercicio04/AppRelogio.java
@@ -2,11 +2,19 @@ package exercicios.exercicio04;
 
 public class AppRelogio {
     public static void main(String[] args) {
-        Relogio relogio = new Relogio(-15, 45, 00);
+        Relogio relogio = new Relogio(15, 45, 00);
 
         System.out.println("Hora atual: " + relogio.getHora());
         relogio.setHora(-70);
         System.out.println("Hora atual: " + relogio.getHora());
+
+        relogio.setMinuto(-8);
+        System.out.println("Minuto atual: " + relogio.getMinuto());
+        
+        relogio.exibir();
+        System.out.println(relogio.horaAtual());
+
+
 
     }
 }
